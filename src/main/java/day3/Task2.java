d08834fe527d8ecb93a38c602c5c55ba4c702522
krabik <krabik504@yandex.ru>
2023-09-13 15:24:32 +0300

    Add Day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double divisible = scanner.nextDouble();
+        double divider = scanner.nextDouble();
 
+        while (divider != 0) {
+            System.out.println(divisible / divider);
+            divisible = scanner.nextDouble();
+            divider = scanner.nextDouble();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int i = 0;
+        while (i++ < 5) {
+            double divisible = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(divisible / divider);
+        }
     }
 }
