bd653f75eb43063f1221beebf5038558251efc30
krabik <krabik504@yandex.ru>
2023-09-13 11:51:50 +0300

    Add Day1 solution

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int value = 10;
+        while (value-- > 0) {
+            System.out.println("JAVA");
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 3;
+//        for(int i = 1; i < 10; i++) {
+//            System.out.printf("%d x %d = %d%n", i, k, i * k);
+//        }
+        int i = 0;
+        while(i++ < 9) {
+            System.out.printf("%d x %d = %d%n", i, k, i * k);
+        }
     }
 }
