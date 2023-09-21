7489e33b19f69ca4cb669f0f728194a8543dbc50
krabik <krabik504@yandex.ru>
2023-09-21 17:17:23 +0300

    Add Day8 solution

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long timeStartStr = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i < 20_001; i++) {
+            str += i + " ";
+        }
+        System.out.println(str);
+        long timeFinishStr = System.currentTimeMillis();
 
+        long timeStartSb = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 20_001; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+        long timeFinishSb = System.currentTimeMillis();
+
+        System.out.println("Выполнение класса String - " + (timeFinishStr -timeStartStr));
+        System.out.println("Выполнние класса StringBuilder - " + (timeFinishSb - timeStartSb));
     }
 }
