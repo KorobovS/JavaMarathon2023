ae6f5e01d1b440770cad67cf8b2519f59bdb6dfb
krabik <krabik504@yandex.ru>
2023-10-06 20:37:39 +0300

    Add Day18 solution

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,5 +1,7 @@
 package day18;
 
+import java.util.Arrays;
+
 /**
  * @author Neil Alishev
  */
@@ -7,13 +9,20 @@ public class Task1 {
     public static void main(String[] args) {
         int[] numbers = {-402, -50, -249, -15, -665, -311};
 
-        System.out.println(recursionSum(numbers, 0));
+        System.out.println(recursionSum(numbers));
+//        System.out.println(recursionSum(numbers, 0));
     }
 
-    public static int recursionSum(int[] numbers, int i) {
-        if (i == numbers.length)
+    public static int recursionSum(int[] arr) {
+        if (0 == arr.length) {
             return 0;
-
-        return numbers[i] + recursionSum(numbers, i + 1);
+        }
+        return arr[arr.length - 1] + recursionSum(Arrays.copyOf(arr, arr.length - 1));
     }
+//    public static int recursionSum(int[] numbers, int i) {
+//        if (i == numbers.length)
+//            return 0;
+//
+//        return numbers[i] + recursionSum(numbers, i + 1);
+//    }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -8,13 +8,22 @@ public class Task2 {
         System.out.println(count7(717771237)); // 5
     }
 
-    public static int count7(int number) {
-        if (number == 0)
+    public static int count7(int i) {
+        if (i == 0)
             return 0;
 
-        if (number % 10 == 7)
-            return 1 + count7(number / 10);
-        else
-            return count7(number / 10);
+        if (i % 10 == 7)
+            return count7(i / 10) + 1;
+        return count7(i / 10);
     }
+
+//    public static int count7(int number) {
+//        if (number == 0)
+//            return 0;
+//
+//        if (number % 10 == 7)
+//            return 1 + count7(number / 10);
+//        else
+//            return count7(number / 10);
+//    }
 }
